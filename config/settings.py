19468b19b74728f09994d186e6c5488b6e59b8f1
cardene <61857866+cardene777@users.noreply.github.com>
2021-04-04 15:22:26 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -143,12 +143,5 @@ if not DEBUG:
     SECRET_KEY = "ol*sk+=(_sy#r#ymu+-#sxpqx85kg$yxz_x71vj^*uai%un4aa"
     ALLOWED_HOSTS = ["127.0.0.1", "45.32.51.171"]
 
-    # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    # EMAIL_HOST = 'smtp.gmail.com'
-    # EMAIL_PORT = 587
-    # EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    # EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-    # EMAIL_USE_TLS = True
-
     STATIC_ROOT = '/usr/share/nginx/html/static'
     MEDIA_ROOT = '/usr/share/nginx/html/media/'
