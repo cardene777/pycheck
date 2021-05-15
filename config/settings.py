4e64e0e703087beb1b080425c7cc7e192b212678
heku777 <chotips08@gmail.com>
2021-05-15 14:27:52 +0900

    #58 change DEBUG=False

--- config/settings.py
+++ config/settings.py
@@ -130,8 +130,8 @@ LOGIN_REDIRECT_URL = 'skill:home'  # ログイン後のリダイレクト先
 LOGOUT_REDIRECT_URL = 'skill:home'  # ログアウト後のリダイレクト先
 
 # デプロイ設定
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 # ローカル用設定
 if DEBUG:
