d0a8fa0c9127f8eb7481991f25d6930013509133
heku777 <chotips08@gmail.com>
2021-06-18 15:22:55 +0900

    #1 heroku deploy

--- config/settings.py
+++ config/settings.py
@@ -164,6 +164,42 @@ if not DEBUG:
 
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        # ログ出力フォーマットの設定
+        'formatters': {
+            'production': {
+                'format': '%(asctime)s [%(levelname)s] %(process)d %(thread)d '
+                          '%(pathname)s:%(lineno)d %(message)s'
+            },
+        },
+        # ハンドラの設定
+        'handlers': {
+            'file': {
+                'level': 'INFO',
+                'class': 'logging.FileHandler',
+                'filename': '/var/log/{}/app.log'.format("config"),
+                'formatter': 'production',
+            },
+        },
+        # ロガーの設定
+        'loggers': {
+            # 自分で追加したアプリケーション全般のログを拾うロガー
+            '': {
+                'handlers': ['file'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+            # Django自身が出力するログ全般を拾うロガー
+            'django': {
+                'handlers': ['file'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+        },
+    }
+
 
 
 
