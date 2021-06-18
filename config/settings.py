e7b8b87c863f63ae6234e541ad237fd87457962d
heku777 <chotips08@gmail.com>
2021-06-18 15:57:50 +0900

    #1 heroku deploy

--- config/settings.py
+++ config/settings.py
@@ -166,38 +166,38 @@ if not DEBUG:
 
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-    LOGGING = {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "handlers": {
-            "file": {
-                "class": "logging.handlers.RotatingFileHandler",
-                "filename": f"{BASE_DIR}/logs/django.log",
-                "formatter": "verbose",
-                "maxBytes": 1024 * 1024 * 1,
-                "backupCount": 5,
-            },
-        },
-        "formatters": {
-            "verbose": {
-                "format": "\t".join(
-                    [
-                        "[%(levelname)s]",
-                        "%(asctime)s",
-                        "%(name)s.%(funcName)s:%(lineno)s",
-                        "%(message)s",
-                    ]
-                )
-            },
-        },
-        "loggers": {
-            "file": {
-                "handlers": ["file"],
-                "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
-                "propagate": True,
-            },
-        },
-    }
+    # LOGGING = {
+    #     "version": 1,
+    #     "disable_existing_loggers": False,
+    #     "handlers": {
+    #         "file": {
+    #             "class": "logging.handlers.RotatingFileHandler",
+    #             "filename": f"{BASE_DIR}/logs/django.log",
+    #             "formatter": "verbose",
+    #             "maxBytes": 1024 * 1024 * 1,
+    #             "backupCount": 5,
+    #         },
+    #     },
+    #     "formatters": {
+    #         "verbose": {
+    #             "format": "\t".join(
+    #                 [
+    #                     "[%(levelname)s]",
+    #                     "%(asctime)s",
+    #                     "%(name)s.%(funcName)s:%(lineno)s",
+    #                     "%(message)s",
+    #                 ]
+    #             )
+    #         },
+    #     },
+    #     "loggers": {
+    #         "file": {
+    #             "handlers": ["file"],
+    #             "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+    #             "propagate": True,
+    #         },
+    #     },
+    # }
 
 
 
