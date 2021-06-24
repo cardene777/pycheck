68956a8a99851936716155f052954b33ba004823
heku777 <chotips08@gmail.com>
2021-06-24 20:05:01 +0900

    #78 update files

--- skill/views.py
+++ skill/views.py
@@ -69,3 +69,20 @@ def export(request):
             [data.pk, data.username, data.question_number, data.question_level,
              data.answer_time, data.score])
     return response
+
+
+def result_export(request):
+    """
+    data export csv file
+    :param request:
+    :return: csv file
+    """
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="data.csv"'
+    # HttpResponseオブジェクトはファイルっぽいオブジェクトなので、csv.writerにそのまま渡せます。
+    writer = csv.writer(response)
+    writer.writerow(["pk", "ユーザ名", "提出数", "合計点", "平均点"])
+    for data in Result.objects.all():
+        writer.writerow(
+            [data.pk, data.username, data.present_number, data.total_points, data.average_point])
+    return response
