23329ccbecd43b3bcd1ac8277f49f513f4fc12c1
heku777 <chotips08@gmail.com>
2021-06-25 09:31:07 +0900

    #78 update files

--- skill/urls.py
+++ skill/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
     path('upload/', views.upload, name='upload'),
     path("upload_done/", views.UploadDone.as_view(), name="upload_done"),
     path("results_register/", views.results_register, name="results_register"),
-    path("export/", views.export, name="export"),
+
+    # export
+    path("export/", views.Export.as_view(), name="export"),
+    path("data_export/", views.data_export, name="data_export"),
     path("result_export/", views.result_export, name="result_export")
 ]
 
